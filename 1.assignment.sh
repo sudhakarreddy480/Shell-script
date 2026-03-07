449af811196cc5f440c194843792eabc21ff110b
sudhakarreddy <sudhakarreddy480@gmail.com>
2026-03-07 19:43:40 +0530

    message

--- 1.assignment.sh
+++ 1.assignment.sh
@@ -3,6 +3,16 @@
 FILE=$1
 PATH=$2
 
+#check for path exist or not
+
+if [ -d $PATH ] ; then
+    echo " $PATH exist now checking for File"
+else
+    echo " $PATH not exist"
+    echo 1
+fi
+
+#check for file exist
 if [ -f  /$PATH/$FILE ] ; then
    echo "$FILE exists in path $PATH"
 else
