e99db7b0f99bc5ec129dd0cc3dceebcf37a61d89
sudhakarreddy <sudhakarreddy480@gmail.com>
2026-03-10 07:34:32 +0530

    message

--- 5.assignment_largest.sh
+++ 5.assignment_largest.sh
@@ -5,4 +5,23 @@
 
 # ./largest.sh 12 56 3 89 42
 # # Output: Largest number is 89
-echo $@
+
+#check for any arguments provided or not
+
+if [ $@ -eq 0 ];then
+    echo "no arguments provided"
+    exit 1
+else
+    echo " Finding the largest number from list $@ "
+fi
+
+max_num=$1
+
+for i in "$@";do
+
+    if [ $i -gt $max_num ];then
+        max_num=$i
+    fi
+done
+
+echo " largest number is $max_num "
